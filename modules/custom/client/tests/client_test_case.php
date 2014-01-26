ff5b49a4c581f00be008ac1f464828e9596ead3c
Pieter Frenssen <pieter@frenssen.be>
2014-01-26 11:48:11 +0100

    Issue #1141: Moved test helper methods to the base class.

--- modules/custom/client/tests/client_test_case.php
+++ modules/custom/client/tests/client_test_case.php
@@ -94,4 +94,91 @@ class ClientTestCase extends DrupalWebTestCase {
     return $this->assertTrue($result, $message ?: 'The client database table is not empty.', $group);
   }
 
+  /**
+   * Check if the properties of the given client match the given values.
+   *
+   * @param Client $client
+   *   The Client entity to check.
+   * @param array $values
+   *   An associative array of values to check, keyed by property name.
+   * @param string $message
+   *   The message to display along with the assertion.
+   * @param string $group
+   *   The type of assertion - examples are "Browser", "PHP".
+   *
+   * @return bool
+   *   TRUE if the assertion succeeded, FALSE otherwise.
+   */
+  public function assertClientProperties(Client $client, array $values, $message = '', $group = 'Other') {
+    return $this->assertEntityProperties('client', $client, $values, $message, $group);
+  }
+
+  /**
+   * Check if the properties of the given entity match the given values.
+   *
+   * @param string $entity_type
+   *   The type of the entity.
+   * @param Entity $entity
+   *   The entity to check.
+   * @param array $values
+   *   An associative array of values to check, keyed by property name.
+   * @param string $message
+   *   The message to display along with the assertion.
+   * @param string $group
+   *   The type of assertion - examples are "Browser", "PHP".
+   *
+   * @return bool
+   *   TRUE if the assertion succeeded, FALSE otherwise.
+   */
+  public function assertEntityProperties($entity_type, Entity $entity, array $values, $message = '', $group = 'Other') {
+    $wrapper = entity_metadata_wrapper($entity_type, $entity);
+
+    $result = TRUE;
+    foreach ($values as $property => $value) {
+      if (is_array($value)) {
+        $result &= $this->assertFalse(array_diff($value, $wrapper->$property->value()), format_string('The %property property has the correct value.', array('%property' => $property)));
+      }
+      else {
+        $result &= $this->assertEqual($wrapper->$property->value(), $value, format_string('The %property property has the correct value.', array('%property' => $property)));
+      }
+    }
+
+    return $this->assertTrue($result, $message ?: format_string('The @entity contains the given values.', array('@entity' => $entity_type)), $group);
+  }
+
+  /**
+   * Creates a new client entity with random values.
+   *
+   * @param array $values
+   *   An optional associative array of values, keyed by property name. Random
+   *   values will be applied to all omitted properties.
+   *
+   * @return Client
+   *   A new client entity.
+   */
+  public function createClient(array $values = array()) {
+    // Provide some default values.
+    $values += $this->randomClientValues();
+    $client = client_create();
+    $this->updateClient($client, $values);
+
+    return $client;
+  }
+
+  /**
+   * Updates the given client with the given properties.
+   *
+   * @param Client $client
+   *   The client entity to update.
+   * @param array $values
+   *   An associative array of values to apply to the entity, keyed by property
+   *   name.
+   */
+  public function updateClient(Client $client, array $values) {
+    $wrapper = entity_metadata_wrapper('client', $client);
+    foreach ($values as $property => $value) {
+      $wrapper->$property->set($value);
+    }
+  }
+
 }

9fb0862b40a1b5a6a2f6abcd1341f19a0f58380f
Pieter Frenssen <pieter@frenssen.be>
2014-02-26 17:16:11 +0100

    Added a script to create some test users.

---
+++ modules/custom/client/create_demo_users.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script will create some demo users to test with:
+# - administrator
+# - authenticated user
+# - business owner
+# - client
+#
+# Use the password "demo" for each of these users.
+#
+# Disclaimer: this is only intended to be used in controlled environments for
+# demonstration purposes. Do not use this on websites that are publicly
+# accessible.
+
+# Execute the drush commands from within the site.
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${ROOT_DIR}"
+
+# Create the users.
+drush user-create administrator --password="demo"
+drush user-create "authenticated user" --password="demo"
+drush user-create "business owner" --password="demo"
+drush user-create client --password="demo"
+
+# Add the roles to the users.
+drush user-add-role administrator administrator
+drush user-add-role "authenticated user" "authenticated user"
+drush user-add-role "business owner" "business owner"
+drush user-add-role client client
+
+exit 0

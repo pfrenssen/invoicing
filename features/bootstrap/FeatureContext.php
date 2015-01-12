76f5e499ed0ecac02d616b88d0caf9b1c2c62006
Pieter Frenssen <pieter@frenssen.be>
2015-01-12 09:43:14 +0100

    Issue #IN-16: Add skeleton for Behat tests.

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains \FeatureContext.
+ */
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Generic step definitions for the Invoicing project.
+ */
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+}

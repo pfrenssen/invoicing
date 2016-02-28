d55b9a25bcb6f00273f2a12d1958add6fcbe80e3
Pieter Frenssen <pieter@frenssen.be>
2016-02-28 12:15:40 +0100

    IN-89: Hack in the requirements for Bulgarian invoices.

--- lib/modules/invoice/theme/invoice_export.tpl.php
+++ lib/modules/invoice/theme/invoice_export.tpl.php
@@ -44,46 +44,29 @@
     <?php print $styles; ?>
   </head>
   <body>
-    <div id="first-column">
-      <?php if ($business_name): ?>
-        <h2 id="business-name"><?php print $business_name; ?></h2>
+  <div id="invoice">
+    <h1><?php print t('Invoice'); ?></h1>
+    <div id="invoice-originality"><?php print t('Original'); ?></div>
+    <div id="invoice-details-left">
+      <?php if ($invoice_number): ?>
+        <div id="invoice-number"><?php print render($invoice_number); ?></div>
       <?php endif; ?>
-      <?php if ($business_address): ?>
-        <div id="business-address"><?php print render($business_address); ?></div>
-      <?php endif; ?>
-      <div id="business-contact">
-        <?php if ($business_email): ?>
-          <div id="business-email"><?php print render($business_email); ?></div>
-        <?php endif; ?>
-        <?php if($business_phone): ?>
-          <div id="business-phone"><?php print render($business_phone); ?></div>
-        <?php endif; ?>
-        <?php if($business_mobile): ?>
-          <div id="business-mobile"><?php print render($business_mobile); ?></div>
-        <?php endif; ?>
-      </div>
-      <?php if($business_vat): ?>
-        <div id="business-vat"><?php print render($business_vat); ?></div>
-      <?php endif; ?>
-      <?php if($business_iban): ?>
-        <div id="business-iban"><?php print render($business_iban); ?></div>
-      <?php endif; ?>
-      <?php if ($business_bic): ?>
-        <div id="business-bic"><?php print render($business_bic); ?></div>
-      <?php endif; ?>
+      <div class="field-label"><?php print t('Place of issue'); ?>:</div>
+      <div id="invoice-place"><?php print t('Sofia'); ?></div>
     </div>
-    <div id="second-column">
-      <div id="invoice">
-        <?php if ($invoice_number): ?>
-          <div id="invoice-number"><?php print render($invoice_number); ?></div>
-        <?php endif; ?>
-        <?php if ($invoice_date): ?>
-          <div id="invoice-date"><?php print render($invoice_date); ?></div>
-        <?php endif; ?>
-      </div>
+    <div id="invoice-details-right">
+      <?php if ($invoice_date): ?>
+        <div id="invoice-date"><?php print render($invoice_date); ?></div>
+      <?php endif; ?>
+      <div class="field-label"><?php print t('Date of tax event'); ?>:</div>
+      <div id="invoice-tax-date"><?php print $invoice_date[0]['#markup']; ?></div>
+    </div>
+  </div>
+    <div id="first-column">
       <div id="client">
-        <div class="field-label"><?php print t('Client'); ?>:</div>
+        <h2><?php print t('Recipient'); ?></h2>
         <?php if ($client_name): ?>
+          <div class="field-label"><?php print t('Company name'); ?>:</div>
           <div id="client-name"><?php print $client_name; ?></div>
         <?php endif; ?>
         <?php if ($client_address): ?>
@@ -97,9 +80,51 @@
             <div id="client-phone"><?php print render($client_phone); ?></div>
           <?php endif; ?>
         </div>
+        <div class="field-label"><?php print t('Company number'); ?>:</div>
+        <div id="client-number">0450.760.087</div>
         <?php if($client_vat): ?>
           <div id="client-vat"><?php print render($client_vat); ?></div>
         <?php endif; ?>
+        <div class="field-label"><?php print t('Accountable'); ?>:</div>
+        <div id="client-accountable">Mathieu De Ruyver</div>
+      </div>
+    </div>
+    <div id="second-column">
+      <div id="business">
+        <h2><?php print t('Supplier'); ?></h2>
+        <?php if ($business_name): ?>
+          <div class="field-label"><?php print t('Company name'); ?>:</div>
+          <div id="business-name"><?php print $business_name; ?></div>
+        <?php endif; ?>
+        <?php if ($business_address): ?>
+          <div id="business-address"><?php print render($business_address); ?></div>
+        <?php endif; ?>
+        <div id="business-contact">
+          <?php if ($business_email): ?>
+            <div id="business-email"><?php print render($business_email); ?></div>
+          <?php endif; ?>
+          <?php if($business_phone): ?>
+            <div id="business-phone"><?php print render($business_phone); ?></div>
+          <?php endif; ?>
+          <?php if($business_mobile): ?>
+            <div id="business-mobile"><?php print render($business_mobile); ?></div>
+          <?php endif; ?>
+        </div>
+        <div class="field-label"><?php print t('Company number'); ?>:</div>
+        <div id="business-number">203805153</div>
+        <?php if($business_vat): ?>
+          <div id="business-vat"><?php print render($business_vat); ?></div>
+        <?php endif; ?>
+        <div class="field-label"><?php print t('Bank'); ?>:</div>
+        <div id="business-bank"><?php print t('Raiffeisen Bank'); ?></div>
+        <?php if($business_iban): ?>
+          <div id="business-iban"><?php print render($business_iban); ?></div>
+        <?php endif; ?>
+        <?php if ($business_bic): ?>
+          <div id="business-bic"><?php print render($business_bic); ?></div>
+        <?php endif; ?>
+        <div class="field-label"><?php print t('Accountable'); ?>:</div>
+        <div id="business-accountable">Pieter Frenssen</div>
       </div>
     </div>
     <div id="tables">
@@ -111,10 +136,25 @@
       <?php endif; ?>
       <?php print $totals; ?>
     </div>
-    <?php if ($invoice_terms): ?>
-      <div id="terms">
-         <?php print render($invoice_terms); ?>
-      </div>
-    <?php endif; ?>
+    <div id="extra">
+      <div class="field-label"><?php print t('In words'); ?>:</div>
+      <div id="invoice-total-words">Todo</div>
+      <div class="field-label"><?php print t('Payment method'); ?>:</div>
+      <div id="invoice-total-words"><?php print t('Bank transfer'); ?></div>
+      <?php if ($invoice_due_date): ?>
+        <div id="invoice-due-date"><?php print render($invoice_due_date); ?></div>
+      <?php endif; ?>
+      <?php if ($invoice_terms): ?>
+        <div id="terms">
+          <?php print render($invoice_terms); ?>
+        </div>
+      <?php endif; ?>
+    </div>
+
+    <div id="signature">
+      <div class="field-label"><?php print t('Prepared by'); ?>:</div>
+      <div id="invoice-issuer">Pieter Frenssen</div>
+      <div class="field-label"><?php print t('Signature'); ?>:</div>
+    </div>
   </body>
 </html>
